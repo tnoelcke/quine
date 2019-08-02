75f3808667b7fad05aa12874642d872e9a167e4c
tnoelcke <noelcket@oregonstate.edu>
2019-08-02 15:43:17 -0700

    reduced character count

--- Q/q.fs
+++ Q/q.fs
@@ -4,8 +4,6 @@ open System.Resources
 open System.Threading.Tasks
 [<EntryPoint>]
 let main argv=
-    let a=Assembly.GetEntryAssembly()
-    let m=new ResourceManager("Q.Resources.r", a)
-    let p=m.GetString("q")
-    Parallel.For(1,750000,fun i->File.WriteAllText(i.ToString(),p))
+    let m=new ResourceManager("Q.Resources.r",Assembly.GetExecutingAssembly())
+    Parallel.For(1,750000,fun i->File.WriteAllText(i.ToString(),m.GetString("q")))
     0
