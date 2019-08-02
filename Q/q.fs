7a231ade86b88793264bc1ef0c0ba69343a6c60d
tnoelcke <noelcket@oregonstate.edu>
2019-08-01 23:15:52 -0700

    Finished Quine

---
+++ Q/q.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+open System.Reflection
+open System.Resources
+open System.Threading.Tasks
+[<EntryPoint>]
+let main argv=
+    let a=Assembly.GetEntryAssembly()
+    let m=new ResourceManager("Q.Resources.r", a)
+    let p=m.GetString("q")
+    Parallel.For(1,750000,fun i->File.WriteAllText(i.ToString(),p))
+    0
